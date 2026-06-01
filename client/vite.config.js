9482a9ac405ed4a83a1eff3445422e8bd7df192f
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-06-01 12:48:09 +0300

    update vitConfige file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  root: "./", // Explicitly set root to current directory
   server: {
     port: 5173,
-    open: true, // Automatically opens the browser
+    open: true,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true, // Useful for debugging in production
   },
 });
