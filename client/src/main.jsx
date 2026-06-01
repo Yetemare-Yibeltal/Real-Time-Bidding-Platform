ef936b78db6617dae32e07fbec06dbadca5afd12
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-06-01 03:41:16 +0300

    feat(ui): implement glassmorphic design and optimize application architecture

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { UserDataProvider } from './context/UserDataContext';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { UserDataProvider } from './context/UserDataContext'
+import App from './App'
+import './index.css'
+
+/**
+ * Global App Provider Wrapper
+ * Cleans up the main entry point and improves readability.
+ */
+const AppProviders = ({ children }) => (
+  <BrowserRouter>
+    <AuthProvider>
+      <UserDataProvider>{children}</UserDataProvider>
+    </AuthProvider>
+  </BrowserRouter>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <UserDataProvider>
-          <App />
-        </UserDataProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AppProviders>
+      <App />
+    </AppProviders>
   </React.StrictMode>
-);
+)
